144b1041f76a9733812d2522dc28c1a184d63aea
Rob Reuss <robreuss@mac.com>
2021-09-08 10:20:33 -0400

    Adjusted NetService version / revert

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueSocket.git",.exact("1.0.0")),
         //.package(url: "https://github.com/Bouke/NetService.git",.upToNextMajor(from: "0.6.0")),
-        .package(url: "https://github.com/Bouke/NetService.git",.exact("0.8.1")),
+        //.package(url: "https://github.com/Bouke/NetService.git",.exact("0.8.1")),
+        .package(url: "https://github.com/Bouke/NetService.git", "0.8.0"..<"0.9.0")
         //.package(url: "https://github.com/robreuss/NetService.git",.upToNextMajor(from: "0.8.2")),
         
     ],
